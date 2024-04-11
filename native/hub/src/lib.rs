4e4437bfd028e8ce939925510030160bf1acbbee
jmook <jwonmook@gmail.com>
2024-04-11 23:59:59 +0900

    🐛 리시버 포트, 전송포트 분리

--- native/hub/src/lib.rs
+++ native/hub/src/lib.rs
@@ -17,14 +17,15 @@ rinf::write_interface!();
 
 async fn main() {
     
-    let forward_ip_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8002);
-    let backward_ip_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8004);
+    let forward_ip_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8001);
+    let self_send_addr = "127.0.0.1:8004"; //2 4
+    let self_recv_addr = "127.0.0.1:8005"; //3 5
+    let backward_ip_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8003); //5 3
 
-    let self_addr = "127.0.0.1:8003";
     let mut buf = [0; 1024];
 
     tokio::spawn(async move {
-        let socket = UdpSocket::bind(self_addr).await.unwrap();
+        let socket = UdpSocket::bind(self_recv_addr).await.unwrap();
         loop {
             let (len, recv_addr) = socket.recv_from(&mut buf).await.unwrap();
             let msg = String::from_utf8_lossy(&buf[..len]);
@@ -54,11 +55,12 @@ async fn main() {
 
     let mut MessageSender: tokio::sync::mpsc::Receiver<rinf::DartSignal<SendMessage>> = SendMessage::get_dart_signal_receiver();
     tokio::spawn(async move {
-        let socket = UdpSocket::bind(self_addr).await.unwrap();
+        let socket = UdpSocket::bind(self_send_addr).await.unwrap();
         while let Some(dart_signal) = MessageSender.recv().await {
             let msg = dart_signal.message;
-            
             let send_msg = msg.contents;
+            
+            println!("will send: {}", send_msg);
             // socket.send(send_msg.as_bytes()).await.unwrap();
             socket.send_to(send_msg.as_bytes(), &forward_ip_addr).await.unwrap();
             socket.send_to(send_msg.as_bytes(), &backward_ip_addr).await.unwrap();

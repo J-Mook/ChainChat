30c71b89db67afd648a1d0ebcebcf63a93ac1edd
jmook <jwonmook@gmail.com>
2024-04-11 20:26:22 +0900

    🎉 first commit

---
+++ lib/main.dart
@@ -0,0 +1,361 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:simple_icons/simple_icons.dart';
+import './messages/generated.dart';
+import './messages/chatmessage.pb.dart';
+
+import 'package:sidebarx/sidebarx.dart';
+import 'provider/themeprovider.dart';
+import 'package:provider/provider.dart';
+import 'dart:io';
+
+
+void main() async {
+  await initializeRust();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider( create: (context) => ThemeProvider(), ),
+      ],
+      child: MainApp()
+    )
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MainBody(),
+        ),
+      ),
+
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: themeProvider.themeMode,
+
+    );
+  }
+}
+
+class MainBody extends StatefulWidget {
+  const MainBody({super.key});
+
+  @override
+  State<MainBody> createState() => _MainBody();
+}
+
+class _MainBody extends State<MainBody> {
+
+  SidebarXController _controller = SidebarXController(selectedIndex: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return Row(
+      children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(5, 5, 0, 5),
+          child: SidebarX(
+            controller: _controller,
+            items: const [
+              SidebarXItem(icon: Icons.person, label: ' Home'),
+              SidebarXItem(icon: SimpleIcons.openai, label: ' RGBpalette'),
+              SidebarXItem(icon: Icons.settings, label: ' Analyse'),
+            ],
+            theme: SidebarXTheme(
+              width: 60,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 76, 119, 140),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              margin: EdgeInsets.only(right: 10),
+            ),
+            extendedTheme: SidebarXTheme(
+              width: 150,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 96, 125, 139),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              margin: EdgeInsets.only(right: 20),
+            ),
+            footerBuilder: (context, extended) {
+              return extended ?
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("DarkMode"),
+                  SizedBox(height: 35,
+                    child: FittedBox(
+                      fit:BoxFit.fitHeight,
+                      child: Switch(
+                        value: themeProvider.isDarkMode,
+                        inactiveTrackColor: Colors.black38,
+                        activeColor: Colors.white38,
+                        onChanged: (value) {
+                          themeProvider.toggleTheme(value);
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ) : IconButton(icon: Icon(Icons.dark_mode), onPressed:() => themeProvider.toggleTheme(!themeProvider.isDarkMode),);
+            },
+          ),
+        ),
+        // ViewerBody(),
+        Expanded(
+          child: AnimatedBuilder(
+            animation: _controller,
+            builder:(context, child) {
+              switch (_controller.selectedIndex) {
+                case 0:
+                  return DefChatRoom();
+                case 1:
+                  return ChatRoom();
+                case 2:
+                  return IPSettingsPage();
+                
+                default:
+                  return DefChatRoom();
+              }
+            },
+          )
+        ),
+      ],
+    );
+  }
+}
+
+class DefChatRoom extends StatefulWidget {
+  const DefChatRoom({super.key});
+
+  @override
+  State<DefChatRoom> createState() => _DefChatRoomState();
+}
+
+class _DefChatRoomState extends State<DefChatRoom> {
+
+  List<String> _chatlist = ["this", "is", "first", "chat"];
+
+  @override
+  Widget build(BuildContext context) {
+    // return Row(
+    //   children: [
+    //     Expanded(
+    //       child: ListView.builder(
+    //         scrollDirection: Axis.horizontal,
+    //         itemCount: _chatlist.length,
+    //         itemBuilder:(context, index) => Text(_chatlist[index]),
+    //       ),
+    //     ),
+    //     Container(
+    //       margin: const EdgeInsets.all(20),
+    //       padding: const EdgeInsets.all(20),
+    //       height: 80,
+    //       decoration: BoxDecoration(
+    //         borderRadius: BorderRadius.circular(24.0),
+    //         color: Colors.white,
+    //       ),
+    //       child: Center(
+    //         child: TextField(),
+    //       ),
+    //     ),
+    //   ],
+    // );
+    return Stack(
+      alignment: Alignment.bottomCenter ,
+      children: [
+        Container(
+          color: Colors.amberAccent,
+          child: ListView(),
+        ),
+        Container(
+          margin: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
+          height: 80,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24.0),
+            color: Colors.white,
+          ),
+          child: Center(
+            child: TextField(),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Pair<T1, T2> {
+  final T1 who;
+  final T2 msg;
+
+  Pair(this.who, this.msg);
+}
+
+class ChatRoom extends StatefulWidget {
+  @override
+  _ChatRoomState createState() => _ChatRoomState();
+}
+
+class _ChatRoomState extends State<ChatRoom> {
+  // final List<String> messages = [];
+  final List<Pair<int, String>> messagesList = [];
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        messagesList.insert(0, Pair(1, _controller.text));
+        // messages.insert(0, _controller.text);
+        _controller.clear();
+        FocusScope.of(context).requestFocus(_focusNode);
+      });
+    }
+  }
+  void _recvMessage(String _who, String _contents) {
+    if (_contents.isNotEmpty) {
+      setState(() {
+        messagesList.insert(0, Pair(2, _contents));
+      });
+    }
+  }
+
+  StreamBuilder _recvStream(){
+    return StreamBuilder(
+      stream: RecvMessage.rustSignalStream,
+      builder: (context, snapshot) {
+        final rustSignal = snapshot.data;
+        if (rustSignal != null) {
+          _recvMessage("2", rustSignal.message.contents);
+        }
+        return Text("");
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat Room'),
+      ),
+      body: ListView.builder(
+        reverse: true,
+        itemCount: messagesList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+              messagesList[index].msg,
+              textAlign: messagesList[index].who == 1 ? TextAlign.right : TextAlign.left,),
+          );
+        },
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: TextField(
+                controller: _controller,
+                focusNode: _focusNode,
+                decoration: InputDecoration(
+                  hintText: 'Enter a message...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                ),
+                onSubmitted: (value){_sendMessage();},
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: _sendMessage,
+            ),
+            // IconButton(
+            //   icon: Icon(Icons.call_received),
+            //   onPressed: _recvMessage,
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class IPSettingsPage extends StatefulWidget {
+  @override
+  _IPSettingsPageState createState() => _IPSettingsPageState();
+}
+
+class _IPSettingsPageState extends State<IPSettingsPage> {
+  List<String> ipAddresses = [];
+  final TextEditingController _ipController = TextEditingController();
+
+  void _addIPAddress() {
+    if (_ipController.text.isNotEmpty) {
+      setState(() {
+        ipAddresses.add(_ipController.text.trim());
+        _ipController.clear();
+      });
+    }
+  }
+
+  void _removeIPAddress(String ip) {
+    setState(() {
+      ipAddresses.remove(ip);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('IP Settings'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _ipController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Add IP Address',
+                hintText: 'Enter valid IP address',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _addIPAddress,
+            child: Text('Add IP Address'),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: ipAddresses.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(ipAddresses[index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => _removeIPAddress(ipAddresses[index]),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ native/hub/src/lib.rs
@@ -0,0 +1,64 @@
+//! This `hub` crate is the
+//! entry point of the Rust logic.
+
+use std::net::{Ipv4Addr, SocketAddr};
+
+// This `tokio` will be used by Rinf.
+// You can replace it with the original `tokio`
+// if you're not targeting the web.
+use tokio_with_wasm::tokio;
+
+mod messages;
+mod sample_functions;
+use messages::chatmessage::*;
+use tokio::net::UdpSocket;
+
+rinf::write_interface!();
+
+async fn main() {
+    
+    let forward_ip_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8002);
+    let backward_ip_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8004);
+
+    let self_addr = "127.0.0.1:8003";
+    let mut buf = [0; 1024];
+
+    tokio::spawn(async move {
+        let socket = UdpSocket::bind(self_addr).await.unwrap();
+        loop {
+            let (len, recv_addr) = socket.recv_from(&mut buf).await.unwrap();
+            let msg = String::from_utf8_lossy(&buf[..len]);
+
+            if forward_ip_addr == recv_addr {
+                socket.send_to(msg.as_bytes(), &backward_ip_addr).await.unwrap();
+            }
+            else if backward_ip_addr == recv_addr {
+                socket.send_to(msg.as_bytes(), &forward_ip_addr).await.unwrap();
+            } else {
+                // this is newbie
+                socket.send_to(msg.as_bytes(), &recv_addr).await.unwrap();
+            }
+
+            println!("Received: {} from {}", String::from_utf8_lossy(&buf[..len]), recv_addr);
+        }
+    });
+
+
+    // let socket = UdpSocket::bind("127.0.0.1:0").await.unwrap();
+    // socket.connect("127.0.0.1:8080").await.unwrap();
+
+    let mut MessageSender: tokio::sync::mpsc::Receiver<rinf::DartSignal<SendMessage>> = SendMessage::get_dart_signal_receiver();
+    tokio::spawn(async move {
+        let socket = UdpSocket::bind(self_addr).await.unwrap();
+        while let Some(dart_signal) = MessageSender.recv().await {
+            let msg = dart_signal.message;
+            
+            let send_msg = msg.contents;
+            // socket.send(send_msg.as_bytes()).await.unwrap();
+            socket.send_to(send_msg.as_bytes(), &forward_ip_addr).await.unwrap();
+            socket.send_to(send_msg.as_bytes(), &backward_ip_addr).await.unwrap();
+            println!("Client sent: {}", send_msg);
+        }
+    });
+
+}
